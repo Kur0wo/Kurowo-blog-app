f4897ff255ae85645b4124c66541f74142253c6b
くろを <nobubusiness7@gmail.com>
2022-09-23 23:55:42 +0900

    seedを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+jon = User.create!(email: 'john@wxample,com', password: 'password')
+emily = User.create!(email: 'emily@example.com', password: 'password')
 
-10.times do
-    Article.create(
+5.times do
+    jon.article.create!(
+        title: Faker::Lorem.sentence(word_count: 5),
+        content: Faker::Lorem.sentence(word_count: 100)
+    )
+end
+
+5.times do
+    emily.article.create!(
         title: Faker::Lorem.sentence(word_count: 5),
         content: Faker::Lorem.sentence(word_count: 100)
     )
