eadc6c33e3e62e5dfe269cc9fb29f05ded4c384f
くろを <nobubusiness7@gmail.com>
2022-09-29 21:07:57 +0900

    display_created_at

--- app/models/article.rb
+++ app/models/article.rb
@@ -27,7 +27,7 @@ class Article < ApplicationRecord
     has_many :likes, dependent: :destroy
     belongs_to :user
 
-    def deisplay_created_at
+    def display_created_at
         I18n.l(self.created_at, format: :default)
     end
 
