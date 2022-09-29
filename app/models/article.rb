eb6b724521cc3d2d810cc24733438912b4edffec
くろを <nobubusiness7@gmail.com>
2022-09-29 21:00:50 +0900

    エラー修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -15,16 +15,13 @@
 #
 class Article < ApplicationRecord
     has_one_attached :eyecatch
+    has_rich_text :content
 
     validates :title, presence: true
-    validates :title, length: { minimum: 2, maximum :100 }
-    validates :title, format: { with: /\A(?!@)/ }
+    validates :title, length: {minimum: 2, maxmum: 100}
+    validates :title, format: {with: /\A(?!\@)/ }
 
     validates :content, presence: true
-    validates :content, length: { minimum: 10 }
-    validates :content, uniqueness: true
-
-    validate :validate_title_and_content_length
 
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
