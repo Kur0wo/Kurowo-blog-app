a358fab9496681532dd2a306acb07ca6e867ddc5
くろを <nobubusiness7@gmail.com>
2022-10-07 13:24:10 +0900

    content カラムを排除

--- app/models/article.rb
+++ app/models/article.rb
@@ -15,7 +15,7 @@
 #
 class Article < ApplicationRecord
     has_one_attached :eyecatch
-
+    has_rich_text :content
     validates :title, presence: true
     validates :title, length: {minimum: 2, maxmum: 100}
     validates :title, format: {with: /\A(?!\@)/ }
